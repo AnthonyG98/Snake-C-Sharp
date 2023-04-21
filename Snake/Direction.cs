7b2cfd551e10c3bd4c5d588615b407aded8fdf7b
gonza <gonz.anthony6@gmail.com>
2023-04-20 20:41:17 -0700

    Add project files.

---
+++ Snake/Direction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Snake
+{
+    public class Direction
+    {
+
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+
+
+        public int RowOffset { get; }
+        public int ColOffset { get; }
+
+        private Direction(int rowOffset, int colOffset)
+        {
+            //BC the contructor is private no other class can create an instance
+            //of the direction class
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffset == direction.ColOffset;
+        }
+        //takes "ColOffset" and "RowOffset" and stores a unique value in a data structure
+        //for the game to refer to in an instant
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ Snake/GameState.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState (int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+
+        public void AddSnake()
+        {
+            //Variable r is the middle of the rows
+            int r = Rows / 2;
+            //Loop runs three times to get three blocks added to the body of the snake
+            for (int c = 1; c <= 3; c++)
+            {
+                //Indicates that the snake is on the grid
+                Grid[r, c] = GridValue.Snake;
+                //Once loop executes AddFirst() adds the three blocks of the snake
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+        private IEnumerable<Position> EmptyPositions()
+            //IEnumerable is like a magic bag that lets you take out each item one at
+            //a time and do something with it, without having to worry about the whole
+            //list at once
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for(int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+            if(empty.Count == 0)
+            {
+                return;
+            }
+            Position pos = empty[random.Next(empty.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.Food;
+        }
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+    } 
+}
